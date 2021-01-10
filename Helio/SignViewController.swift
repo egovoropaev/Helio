40eb4ce1afe192691052f2dc7591dd02e46bd389
Ilya <ego.voropaev@gmail.com>
2021-01-10 10:43:40 +0300

    SignViewController
    Added SignViewController

---
+++ Helio/SignViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignViewController.swift
+//  Helio
+//
+//  Created by Илья on 10.01.2021.
+//
+
+import SwiftUI
+
+struct SignIn: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews_one: PreviewProvider {
+    static var previews: some View {
+        SignIn()
+    }
+}
